55196208b3d790e47ab578e1eb793129d6745bac
Hung Nguyen <hungna.dev@gmail.com>
2024-09-22 02:03:00 +0700

    Update MySQLiBaseModel.php

--- src/MySQLiBaseModel.php
+++ src/MySQLiBaseModel.php
@@ -23,8 +23,8 @@ class MySQLiBaseModel
 {
     use Support;
 
-    const VERSION = '3.1.2';
-    const LAST_MODIFIED = '2023-02-15';
+    const VERSION = '3.1.3';
+    const LAST_MODIFIED = '2024-09-22';
     const AUTHOR_NAME = 'Hung Nguyen';
     const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
     const PROJECT_NAME = 'Database Wrapper - MySQLi Database Model';
